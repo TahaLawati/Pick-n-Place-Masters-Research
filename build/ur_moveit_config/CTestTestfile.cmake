a97be5fca7a129ab7523f74c9a9df95dd5d286e3
TahaLawati <tahalawati07@gmail.com>
2024-09-05 14:00:25 +1000

    reinstalled driver
    it was not building before.

---
+++ build/ur_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/taha/workspace/Vision-Grasping-Masters-Research/src/Universal_Robots_ROS2_Driver/ur_moveit_config
+# Build directory: /home/taha/workspace/Vision-Grasping-Masters-Research/build/ur_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/ur_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/taha/workspace/Vision-Grasping-Masters-Research/install/ur_description/share/ur_description/package.sh"
